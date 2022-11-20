db148f0d1e6172816cfd141373703b76822c380c
divya <kamathd37@gmail.com>
2022-11-20 18:18:58 -0500

    added server

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const userRoutes = require("./users");
+const postRoutes = require("./posts");
+const path = require("path");
+
+const constructorMethod = (app) => {
+  app.use("/", (req, res) => {
+    res.sendFile(path.resolve("static/index.html"));
+  });
+  app.use("/posts", postRoutes);
+  app.use("/users", userRoutes);
+
+  app.use("*", (req, res) => {
+    res.sendStatus(404);
+  });
+};
+
+module.exports = constructorMethod;
